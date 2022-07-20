30200e82e0721964a18a069ee0be4ac87d43f9fa
Behzad Samadi <behzad.samadi@gmail.com>
2022-07-20 15:44:07 +0000

    Adding a second test

--- tests/angles_unittest.py
+++ tests/angles_unittest.py
@@ -4,12 +4,29 @@ from app.is_glare import is_glare
 
 
 class AnglesTest(unittest.TestCase):
-    def test_is_glare(self):
+    def test_01(self):
         payload = {
             "lat": 49.2699648,
             "lon": -123.1290368,
             "epoch": 1588704959.321,
             "orientation": -10.2,
+        }
+
+        # Calculate the position of the sun
+        sun = sun_pos(payload)
+
+        self.assertEqual(
+            is_glare(sun, payload["orientation"]),
+            "false",
+            msg="glare should be false",
+        )
+
+    def test_02(self):
+        payload = {
+            "lat": 49.2699648,
+            "lon": -123.1290368,
+            "epoch": 1588704959.321,
+            "orientation": 349.8,
         }
 
         # Calculate the position of the sun
