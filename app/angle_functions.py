cb1225ade59020ba6ba0ce314df7bab7b55626ac
Behzad Samadi <behzad.samadi@gmail.com>
2022-07-20 12:47:52 +0000

    normalize_angle

---
+++ app/angle_functions.py
@@ -0,0 +1,12 @@
+def normalize_angle(angle):
+    # reduce the angle  
+    angle = angle % 360; 
+    
+    # angle in [0,360)
+    angle = (angle + 360) % 360;  
+    
+    # angle in (-180,180]
+    if (angle > 180):
+        angle -= 360
+
+    return angle
--- app/is_glare.py
+++ app/is_glare.py
@@ -1,10 +1,15 @@
-def is_glare(sun,orientation):
+from app.angle_functions import normalize_angle
+
+def is_glare(sun, orientation):
     altitude_sun = sun['altitude'] # Sun's altitude (deg)
     azimuth_sun = sun['azimuth']   # Sun's azimuth (deg)
 
+    heading = normalize_angle(orientation - azimuth_sun)
+    altitude = normalize_angle(altitude_sun)
+
     # There is a possibility of a direct glare if the azimuthal difference between the sun and the direction of the car travel (and hence 
     # the direction of forward-facing camera) is less than 30 degrees and the altitude of the sun is less than 45 degrees.
-    if abs(orientation - azimuth_sun) <= 30.0 and 45.0 >= altitude_sun >= 0:
+    if abs(heading) <= 30.0 and 45.0 >= altitude >= 0:
        glare="true"
     else:
        glare="false"
